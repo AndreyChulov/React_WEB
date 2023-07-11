f41d3793d22758a54b3875a294e94e9eee2efc8a
AndreyChulov <andreyc253@gmail.com>
2023-07-11 22:25:10 +0300

    First unit update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  function increment() {
+    setValue(value + 1);
+    document.getElementById("digit").innerText = value.toString();
+  }
+
+  function decrement() {
+    setValue(value - 1);
+    document.getElementById("digit").innerText = value.toString();
+  }
+
+  function clear() {
+    setValue(0);
+    document.getElementById("digit").innerText = value.toString();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ul>
+          <li>Число:<span id="digit">0</span></li>
+          <li><button onClick={increment}>+</button></li>
+          <li><button onClick={decrement}>-</button></li>
+          <li><button onClick={clear}>0</button></li>
+      </ul>
   );
 }
 
